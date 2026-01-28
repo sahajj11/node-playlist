94182fdf3df9f5919712ff1e89e21ac3cc4bd0d8
Sahaj Rajput <sahajrajput1112@gmail.com>
2026-01-28 17:31:43 +0530

    Implemented GET video APIs

--- node-server/index.js
+++ node-server/index.js
@@ -4,6 +4,16 @@ const app=express()
 
 const port = 3000
 
+app.get("/api/users",(req,res)=>{
+  res.send([{id:3,name:'sahaj'},{id:4,name:"rohan"}])
+})
+
+
+
+app.get("/",(req,res)=>{
+  res.send("server is running ....")
+})
+
 app.listen(port,()=>{
     console.log("server is started...")
 })
