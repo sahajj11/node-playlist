0c2738532733615cfbf174d140eb29be985c1475
Sahaj Rajput <sahajrajput1112@gmail.com>
2026-01-29 16:28:44 +0530

    Implemented req.query and req.params api's

--- node-server/index.js
+++ node-server/index.js
@@ -8,7 +8,15 @@ app.get("/api/users",(req,res)=>{
   res.send([{id:3,name:'sahaj'},{id:4,name:"rohan"}])
 })
 
+app.get("/api/greet",(req,res)=>{
+  const name=req.query.name || "guest"
+  res.send(`hello,${name}`)
+})
 
+app.get("/api/user/:id",(req,res)=>{
+  const {id}=req.params
+  res.send(`welcome ${id}`)
+})
 
 app.get("/",(req,res)=>{
   res.send("server is running ....")
